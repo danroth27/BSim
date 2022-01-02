3c71b1bd2eb9b900fbd80759dc95342d2772d40f
Daniel Roth <daroth@microsoft.com>
2022-01-01 22:56:19 -0800

    Fix proximity sensor exit logic

--- Assets/ProximitySensorController.cs
+++ Assets/ProximitySensorController.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ProximitySensorController : MonoBehaviour
 {
     SpriteRenderer spriteRenderer;
+    Collider2D proximitySensorCollider2D;
 
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        proximitySensorCollider2D = GetComponent<Collider2D>();
     }
 
     // Update is called once per frame
@@ -27,6 +30,11 @@ public class ProximitySensorController : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collider2D)
     {
         if (collider2D.isTrigger) return;
+
+        var contacts = new List<Collider2D>();
+        proximitySensorCollider2D.GetContacts(contacts);
+        if (contacts.Any(c2d => !c2d.isTrigger)) return;
+
         spriteRenderer.color = Color.white;
     }
 }
