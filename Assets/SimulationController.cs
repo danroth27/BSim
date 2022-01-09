17c2959553bd0cf98dd5777d11aef8ba36cf27a6
Daniel Roth <daroth@microsoft.com>
2022-01-08 18:22:44 -0800

    Simulation controls (except Reset)

---
+++ Assets/SimulationController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimulationController : MonoBehaviour
+{
+    public Button startButton, stopButton, stepButton, resetButton;
+    private bool isStepping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log($"{Time.fixedTime}: FixedUpdate");
+        if (isStepping)
+        {
+            Time.timeScale = 0;
+            isStepping = false;
+        }
+    }
+
+    public void StartSimulation()
+    {
+        Time.timeScale = 1;
+        startButton.interactable = false;
+        stopButton.interactable = true;
+        stepButton.interactable = false;
+        resetButton.interactable = false;
+    }
+
+    public void StopSimulation()
+    {
+        Time.timeScale = 0;
+        startButton.interactable = true;
+        stopButton.interactable = false;
+        stepButton.interactable = true;
+    }
+
+    public void StepSimulation()
+    {
+        Debug.Log($"{Time.fixedTime}: StepSimulation");
+        isStepping = true;
+        Time.timeScale = 1;
+    }
+
+    public void ResetSimulation()
+    {
+
+    }
+}
